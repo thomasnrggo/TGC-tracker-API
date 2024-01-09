f7b20a317c7ed5991d679db935fe9b1f6f3144db
Anthony Gonzalez <thomasnrggo@gmail.com>
2024-01-09 13:49:25 -0600

    update API entrypoint message

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to TGC Tracker API!');
+    res.send('Welcome to TGC Tracker API!, api has been initialized!');
 });
 
 mongoose.connect(connection.uri)
